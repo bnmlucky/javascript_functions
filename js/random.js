8412ee7ef418d8e7f0f1617dddc67cb20c5fcfed
Natalia Titova <bnmlucky@gmail.com>
2020-05-04 12:28:31 -0400

    created a function that accepts a parameter

--- js/random.js
+++ js/random.js
@@ -1,9 +1,13 @@
-function getRandomNumber() {
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
+function getRandomNumber(upper) {
+    const randomNumber = Math.floor(Math.random() * upper) + 1;
     return randomNumber;
 }
 
-const dieRoll = getRandomNumber();
+console.log(getRandomNumber(6));
+console.log(getRandomNumber(100));
+console.log(getRandomNumber(1000));
+console.log(getRandomNumber(20));
+
 
 
 
