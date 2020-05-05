ba3d0a1325f72149da3faf0ac9581eb572a16ab2
Natalia Titova <bnmlucky@gmail.com>
2020-05-04 20:41:56 -0400

    did an exercise on variable scope

--- js/scope.js
+++ js/scope.js
@@ -1,4 +1,18 @@
+//Global scope
+const person = 'Lee';
+
 function greeting() {
-  let person = 'Meg';
+  //Function scope
+  const person = 'Meg';
   alert(`Hi, ${person}!`);
 }
+
+function greeting2() {
+  //Function scope
+  let person = 'Robert';
+  alert(`Good morning, ${person}!`);
+}
+
+greeting();
+alert(`Hi, ${person}!`);
+greeting2();
